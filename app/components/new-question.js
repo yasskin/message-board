fa123b393d34bc1aac85fd525f15bf671514381c
Noah <yasskin@icloud.com>
2016-08-18 19:31:21 -0700

    add new question component with route and hbs form

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    showQuestionForm() {
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get('content') ? this.get('content') : "",
+        author: this.get('author') ? this.get('author') : "",
+        note: this.get('note') ? this.get('note') : "",
+        tag: this.get('tag') ? this.get('tag') : ""
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
   actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
