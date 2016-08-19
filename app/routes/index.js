39ff3459a4c48c6cc252c094e1043ac23a6ee8f4
Noah <yasskin@icloud.com>
2016-08-19 08:54:08 -0700

    add Question route with dynamic segments

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
   actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
