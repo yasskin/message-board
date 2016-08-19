7fc82b4b2a4d703e2ce54b100c80b51b935496a5
Noah <yasskin@icloud.com>
2016-08-18 19:15:21 -0700

    add destroy a record delete button and action

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
